59e4fe4efc385473db436f6cfd113a0129a1785c
maksbernatskyy <maksbernatskyy@gmail.com>
2026-01-09 12:27:20 +0100

    feat: implement DetailPage with SingleCard component and fetch product data

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router";
+import SingleCard from "../components/SingleCard";
+
 export default function DetailPage() {
-    return (
-        <>
-        <h1>
-            detail page
-        </h1>
-        </>
-    )
+  const { slug } = useParams();
+  const [todos, setTodos] = useState([]);
+
+  function fetchTodos() {
+    axios
+      .get(`http://localhost:3000/api/products/${slug}`)
+      .then((res) => setTodos(res.data));
+  }
+
+  useEffect(fetchTodos, []);
+  console.log(todos);
+
+  return (
+    <>
+      <SingleCard key={todos.id} todo={todos} />
+    </>
+  );
 }
