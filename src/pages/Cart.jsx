d6937754c24176b017275b17c74e064e67ff18b4
BanLowi <lowi40k@gmail.com>
2026-01-21 18:50:08 +0100

    refactor Cart component layout and styling for improved structure and user experience

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -111,7 +111,7 @@ export default function Cart() {
 
                 <div className="align-self-center ms-3">
                   <button
-                    className="btn btn-danger btn-remove"
+                    className="btn btn-danger btn-remove btn-position"
                     onClick={() => removeProd(product)}
                   >
                     <i className="bi bi-x-lg" />
@@ -123,27 +123,39 @@ export default function Cart() {
         )}
 
         {cart.length > 0 && (
-          <div className="mt-4">
-            <h4 className="text-white">
-              Totale:{discountedTotal}€
-            </h4>
-            <form onSubmit={handleSubmit}>
-              <div className="input-group d-flex">
-                <input className="discount-code form-control" type="text" placeholder="Codice sconto"
-                  onChange={e => setDiscountCode(e.target.value)} />
-                <button type="submit" className="btn-discount"><i className="bi bi-bag-check"></i></button>
-              </div>
-            </form>
+          <div className="mt-4 d-flex justify-content-between align-items-start recap">
+
+            {/* SINISTRA: totale + sconto */}
+            <div>
+              <h4 className="text-white mb-3">
+                Totale: {discountedTotal}€
+              </h4>
+
+              <form onSubmit={handleSubmit}>
+                <div className="input-group d-flex">
+                  <input
+                    className="discount-code form-control"
+                    type="text"
+                    placeholder="Codice sconto"
+                    onChange={e => setDiscountCode(e.target.value)}
+                  />
+                  <button type="submit" className="btn-discount">
+                    <i className="bi bi-bag-check"></i>
+                  </button>
+                </div>
+              </form>
+            </div>
+
+            {/* DESTRA: checkout */}
+            <div className="align-self-center">
+              <Link to="/checkout" className="btn btn-light">
+                Procedi al checkout
+              </Link>
+            </div>
+
           </div>
         )}
 
-        {cart.length > 0 &&
-          <div>
-            <Link to="/checkout" className="btn btn-light">
-              Procedi al checkout
-            </Link>
-          </div>
-        }
       </div>
     </>
   );
